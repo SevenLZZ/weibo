677040bf57c226959c3db39202be14e2228862c3
SevenLZZ <1360066130@qq.com>
2019-08-01 15:56:23 +0000

    CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,14 +11,27 @@ class UsersController extends Controller
     	return view('users.create');
     }
 
-    // public function show(User $user){
+	/**
+	*
+	Laravel 会自动解析定义在控制器方法（变量名匹配路由片段）中的 Eloquent 模型类型声明。在上面代码中，由于 show() 方法传参时声明了类型 —— Eloquent 模型 User，对应的变量名 $user 会匹配路由片段中的 {user}，这样，Laravel 会自动注入与请求 URI 中传入的 ID 对应的用户模型实例。
 
-    // 	return view('users.show',compact('user'));
-    // }
+	此功能称为 『隐性路由模型绑定』，是『约定优于配置』设计范式的体现，同时满足以下两种情况，此功能即会自动启用：
 
+	**/
+    //users/1 满足条件，直接查找id为1的数据
  	public function show(User $user)
     {
-    	
+
         return view('users.show', compact('user'));
+    }
+
+     public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
